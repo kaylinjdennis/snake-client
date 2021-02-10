89ddeeed57c19f70ffffdc47e343b2f2516a5471
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-10 14:57:18 -0700

    refactored by moving connect function into client.js

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+// Establishes connection with game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('data', () => {
+    console.log('you ded cuz you idled');
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  return conn;
+};
+
+module.exports = { connect };
