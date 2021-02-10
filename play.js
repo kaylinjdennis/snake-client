2a4c282570c7a17e0ab82d814584ceecbe57af0c
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-10 14:49:33 -0700

    create client

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+// Establishes connection with game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('data', () => {
+    console.log('you ded cuz you idled');
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
