98a5658af3f8e42501ce0c0833b3b3a5bc019013
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-10 15:49:14 -0700

    refactored by moving 2 functions into a separate file

---
+++ input.js
@@ -0,0 +1,21 @@
+// Set up User Interface
+// So we can handle user input via stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const { connect } = require('./client');
-
-// Set up User Interface
-// So er can handle user input via stdin
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-setupInput();
 connect();
+
+setupInput();
