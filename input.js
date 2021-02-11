a5e3cfc8428f1a4c860985a58332c6d39be3696d
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-10 18:56:35 -0700

    added functionality to move snake

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
+// Stores the active TCP connection object.
+let connection;
+
 // Set up User Interface
 // So we can handle user input via stdin
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -15,6 +19,14 @@ const handleUserInput = (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
